4e3820b6d54cc1f71aac4f46d4a6f81aaff34ec5
Myungji Choi <choimyungji@gmail.com>
2019-01-31 23:55:31 +0900

    initialize project

---
+++ BetterTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BetterTimer
+//
+//  Created by Myungji Choi on 31/01/2019.
+//  Copyright © 2019 Myungji Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
