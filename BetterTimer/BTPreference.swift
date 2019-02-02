0de6b2be27f725d49fb6936f09db265082e3e929
Myungji Choi <choimyungji@gmail.com>
2019-02-02 23:43:41 +0900

    add UserDefaults wrapper

---
+++ BetterTimer/BTPreference.swift
@@ -0,0 +1,22 @@
+//
+//  BTPreference.swift
+//  BetterTimer
+//
+//  Created by Myungji Choi on 02/02/2019.
+//  Copyright © 2019 Myungji Choi. All rights reserved.
+//
+
+import UIKit
+
+class BTPreference: NSObject {
+  let userDefinedTimeKey = "userDefinedTime"
+  
+  var userPhotoURL: Int {
+    set (newVal) {
+      UserDefaults().set(newVal, forKey: userDefinedTimeKey)
+    }
+    get {
+      return UserDefaults().object(forKey: userDefinedTimeKey) as? Int ?? 120
+    }
+  }
+}
