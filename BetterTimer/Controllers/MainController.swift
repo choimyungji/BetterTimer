7fd52277b27298ecb9c07371f0f0a5404e03ff41
Myungji Choi <choimyungji@gmail.com>
2019-08-28 19:22:14 +0900

    add viewmodel initializer

--- BetterTimer/Controllers/MainController.swift
+++ BetterTimer/Controllers/MainController.swift
@@ -9,7 +9,18 @@
 import UIKit
 import UserNotifications
 
-class MainController: UIViewController {
+final class MainController: UIViewController {
+  init(_ viewModel: MainViewModel) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  let viewModel: MainViewModel
+
   var userDefinedTime: Date?
   let defaultMargin: CGFloat = 24
   var isShownViewComponent: Bool = true
