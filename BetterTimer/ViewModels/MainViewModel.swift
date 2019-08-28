5cb03f098f052b85e1178be49aa81ce3f9c11666
Myungji Choi <choimyungji@gmail.com>
2019-08-28 19:22:00 +0900

    add main viewmodel

---
+++ BetterTimer/ViewModels/MainViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewModel.swift
+//  BetterTimer
+//
+//  Created by Myungji Choi on 28/08/2019.
+//  Copyright © 2019 Myungji Choi. All rights reserved.
+//
+
+import Foundation
+
+protocol MainViewModelType: class {
+
+}
+
+class MainViewModel: MainViewModelType {
+  
+}
