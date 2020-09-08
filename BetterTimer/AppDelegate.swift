80a17fbdf7caf762ece4ccdab2f7f603bec933b5
Myungji Choi <choimyungji@gmail.com>
2020-09-08 23:46:08 +0900

    SceneDelegate 추가 및 SwiftUI메인뷰 추가

--- BetterTimer/AppDelegate.swift
+++ BetterTimer/AppDelegate.swift
@@ -16,9 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    window = UIWindow(frame: UIScreen.main.bounds)
-    appCoordinator = window.flatMap(AppCoordinator.init)
-    appCoordinator?.start()
     return true
   }
 
---
+++ BetterTimer/View/MainView.swift
@@ -0,0 +1,24 @@
+//
+//  MainView.swift
+//  BetterTimer
+//
+//  Created by Myungji Choi on 2020/09/08.
+//  Copyright © 2020 Myungji Choi. All rights reserved.
+//
+
+import Foundation
+
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
