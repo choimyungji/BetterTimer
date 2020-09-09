02c46c48fe00709616f2b6285e6bc53b8f601a2e
Myungji Choi <choimyungji@gmail.com>
2020-09-09 23:40:35 +0900

    new mainView with SwiftUI

--- BetterTimer/View/MainView.swift
+++ BetterTimer/View/MainView.swift
@@ -8,12 +8,34 @@
 
 import Foundation
 
-
 import SwiftUI
 
 struct MainView: View {
+    @EnvironmentObject var viewModel: MainViewModel
+    @State var degree: Double = 90
+
+//    let viewModel: MainViewModel
+//    init(_ degree: Double) {
+//        self.degree = degree
+//    }
+
     var body: some View {
-        Text("Hello, World!")
+        MainArcShape(degree: degree).foregroundColor(.red)
+    }
+}
+
+struct MainArcShape: Shape {
+    @State var degree: Double
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.maxX / 2, y: rect.maxY / 2),
+                    radius: (rect.width - (46 * 2)) / 2,
+                    startAngle: .degrees(90),
+                    endAngle: .degrees(degree),
+                    clockwise: true)
+
+        return path
+            .strokedPath(StrokeStyle(lineWidth: 100))
     }
 }
 
